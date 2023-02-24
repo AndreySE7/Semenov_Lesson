837ad632c118a29919ca180cef298129f168c7fd
AndreySE7 <drey_ac@mail.ru>
2023-02-24 14:52:47 +0300

    2nd commit

--- app/src/main/java/com/example/semenov_lesson/MainActivity.kt
+++ app/src/main/java/com/example/semenov_lesson/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.semenov_lesson
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.semenov_lesson.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,19 +16,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.contactsButton.setOnClickListener {
-            binding.progressGroup.visibility = View.GONE
-            binding.progressBar.visibility = View.VISIBLE
+        val time = Time(0, 0, 0)
+        thread {
+            while (true) {
+                val currTime = Calendar.getInstance()
+                val hour = currTime.get(Calendar.HOUR)
+                val minutes = currTime.get(Calendar.MINUTE)
+                val seconds = currTime.get(Calendar.SECOND)
+                time.setTime(hour, minutes, seconds)
+                runOnUiThread {
+                    binding.watchView.setNewTime(time)
+                }
+                Thread.sleep(1000)
+            }
         }
 
-        binding.vacanciesButton.setOnClickListener {
-            binding.progressGroup.visibility = View.GONE
-            binding.progressBar.visibility = View.VISIBLE
-        }
-
-        binding.galleryButton.setOnClickListener {
-            binding.progressGroup.visibility = View.GONE
-            binding.progressBar.visibility = View.VISIBLE
-        }
     }
 }
